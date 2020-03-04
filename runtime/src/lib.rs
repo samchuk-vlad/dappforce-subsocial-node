9a2973e18ef307c6974fc09e001079e599ed5725
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-03-04 14:07:41 +0200

    Add pallet-utility to runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -234,6 +234,23 @@ impl pallet_social::Trait for Runtime {
 	type Event = Event;
 }
 
+parameter_types! {
+	// One storage item; value is size 4+4+16+32 bytes = 56 bytes.
+	pub const MultisigDepositBase: Balance = 30;
+	// Additional storage item size of 32 bytes.
+	pub const MultisigDepositFactor: Balance = 5;
+	pub const MaxSignatories: u16 = 100;
+}
+
+impl pallet_utility::Trait for Runtime {
+	type Event = Event;
+	type Call = Call;
+	type Currency = Balances;
+	type MultisigDepositBase = MultisigDepositBase;
+	type MultisigDepositFactor = MultisigDepositFactor;
+	type MaxSignatories = MaxSignatories;
+}
+
 construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
@@ -249,6 +266,7 @@ construct_runtime!(
 		TransactionPayment: transaction_payment::{Module, Storage},
 		Sudo: sudo,
 		Social: pallet_social::{Module, Call, Storage, Event<T>},
+		Utility: pallet_utility::{Module, Call, Storage, Event<T>},
 		RandomnessCollectiveFlip: randomness_collective_flip::{Module, Call, Storage},
 	}
 );
