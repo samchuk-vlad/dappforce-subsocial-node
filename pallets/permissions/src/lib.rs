00f7beb82ebbc8833041a5d3b65b5f03b664638f
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-05-30 14:59:46 +0300

    WIP: Refactor Permissions
    Co-Authored-By: siman <aleksandr.siman@gmail.com>

--- pallets/permissions/src/lib.rs
+++ pallets/permissions/src/lib.rs
@@ -5,7 +5,10 @@ use sp_std::collections::{
   btree_map::BTreeMap
 };
 use codec::{Encode, Decode};
-use frame_support::{decl_module, traits::Get};
+use frame_support::{
+  decl_module,
+  traits::Get
+};
 use sp_runtime::RuntimeDebug;
 
 #[derive(Encode, Decode, Ord, PartialOrd, Clone, Eq, PartialEq, RuntimeDebug)]
@@ -109,5 +112,40 @@ decl_module! {
   pub struct Module<T: Trait> for enum Call where origin: T::Origin {
     const DefaultSpacePermissions: SpacePermissions = T::DefaultSpacePermissions::get();
     const DefaultPostPermissions: PostPermissions = T::DefaultPostPermissions::get();
+  }
+}
+
+impl<T: Trait> Module<T> {
+
+  pub fn has_user_a_space_permission(
+    is_owner: bool,
+    is_follower: bool,
+    overrides: SpacePermissions,
+    permission: SpacePermission,
+  ) -> bool {
+
+    // Try to find a permission in space overrides:
+    let mut role_opt = overrides.get(&permission);
+
+    // Look into default space permissions,
+    // if there is no permission override for this space:
+    let default_perms = T::DefaultSpacePermissions::get();
+    if role_opt.is_none() {
+      role_opt = default_perms.get(&permission);
+    }
+
+    if let Some(role) = role_opt {
+      if *role == BuiltinRole::None {
+        return false;
+      } else if
+        *role == BuiltinRole::Owner && is_owner ||
+        *role == BuiltinRole::Follower && (is_owner || is_follower) ||
+        *role == BuiltinRole::Everyone
+      {
+        return true;
+      }
+    }
+
+    false
   }
 }
