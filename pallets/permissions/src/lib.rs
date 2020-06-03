17eb238f7a1f6076a505b7c430662d7be1a9314a
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-06-03 13:04:04 +0300

    WIP: New Space/Post permissions implementations

--- pallets/permissions/src/lib.rs
+++ pallets/permissions/src/lib.rs
@@ -1,8 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use sp_std::prelude::*;
-use sp_std::collections::{
-  btree_map::BTreeMap
+use sp_std::{
+  prelude::*,
+  collections::btree_set::BTreeSet,
+  iter::IntoIterator
 };
 use codec::{Encode, Decode};
 use frame_support::{
@@ -109,23 +110,23 @@ impl Into<SpacePermission> for PostPermission {
   }
 }
 
-/*
-Example of using built-in roles with permissions:
+#[derive(Encode, Decode, Clone, Eq, PartialEq, RuntimeDebug)]
+pub struct SpacePermissions {
+  none: Option<SpacePermissionSet>,
+  everyone: Option<SpacePermissionSet>,
+  follower: Option<SpacePermissionSet>,
+  space_owner: Option<SpacePermissionSet>,
+  post_owner: Option<PostPermissionSet>
+}
 
-                  None    Owner    Follower    Everyone
--------------------------------------------------------
-CreatePosts    |            X
--------------------------------------------------------
-CreateComments |                      X
--------------------------------------------------------
-Share          |                      X
--------------------------------------------------------
-Upvote         |                                  X
--------------------------------------------------------
-Downvote       |   X
--------------------------------------------------------
-
-*/
+#[derive(Encode, Decode, Clone, Eq, PartialEq, RuntimeDebug)]
+pub struct PostPermissions {
+  none: Option<PostPermissionSet>,
+  everyone: Option<PostPermissionSet>,
+  follower: Option<PostPermissionSet>,
+  space_owner: Option<PostPermissionSet>,
+  post_owner: Option<PostPermissionSet>
+}
 
 #[derive(Encode, Decode, Ord, PartialOrd, Clone, Eq, PartialEq, RuntimeDebug)]
 pub enum BuiltinRole {
@@ -133,31 +134,119 @@ pub enum BuiltinRole {
   /// None is allowed.
   None,
 
-  /// An owner of this entity on which we are checking a permission.
-  /// For example it could be an owner of a space or a post (comment).
-  Owner,
+  /// An owner of the space on which we are checking a permission.
+  SpaceOwner,
 
   /// Owners and followers of this space allowed.
   Follower,
 
   /// Every user of this blockchain is allowed.
   Everyone,
+
+  /// An owner of the post on which we are checking a permission.
+  PostOwner,
 }
 
-pub type SpacePermissions = BTreeMap<SpacePermission, BuiltinRole>;
+impl IntoIterator for SpacePermissions {
+  type Item = SpacePermissionSet;
+  type IntoIter = sp_std::vec::IntoIter<Self::Item>;
 
-pub type PostPermissions = BTreeMap<PostPermission, BuiltinRole>;
+  fn into_iter(self) -> Self::IntoIter {
+    let mut post_owner_converted = SpacePermissionSet::new();
+    if let Some(post_owner) = self.post_owner {
+      for permission in post_owner.iter() {
+        post_owner_converted.insert(permission.clone().into());
+      }
+    }
+
+    vec![
+      self.none.unwrap_or(SpacePermissionSet::new()),
+      self.space_owner.unwrap_or(SpacePermissionSet::new()),
+      self.follower.unwrap_or(SpacePermissionSet::new()),
+      self.everyone.unwrap_or(SpacePermissionSet::new()),
+      post_owner_converted
+    ].into_iter()
+  }
+}
+
+impl IntoIterator for PostPermissions {
+  type Item = PostPermissionSet;
+  type IntoIter = sp_std::vec::IntoIter<Self::Item>;
+
+  fn into_iter(self) -> Self::IntoIter {
+    vec![
+      self.none.unwrap_or(PostPermissionSet::new()),
+      self.space_owner.unwrap_or(PostPermissionSet::new()),
+      self.follower.unwrap_or(PostPermissionSet::new()),
+      self.everyone.unwrap_or(PostPermissionSet::new()),
+      self.post_owner.unwrap_or(PostPermissionSet::new())
+    ].into_iter()
+  }
+}
+
+impl From<usize> for BuiltinRole {
+  fn from(v: usize) -> BuiltinRole {
+    match v {
+      0 => BuiltinRole::None,
+      1 => BuiltinRole::SpaceOwner,
+      2 => BuiltinRole::Follower,
+      3 => BuiltinRole::Everyone,
+      4 => BuiltinRole::PostOwner,
+      _ => BuiltinRole::None
+    }
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/*impl BuiltinRole {
+  fn get_space_permissions_set_by_role(&self, space_perms: SpacePermissions) -> SpacePermissionSet {
+    match self {
+      BuiltinRole::None => space_perms.none.unwrap_or(SpacePermissionSet::new()),
+      BuiltinRole::SpaceOwner => space_perms.space_owner.unwrap_or(SpacePermissionSet::new()),
+      BuiltinRole::Follower => space_perms.follower.unwrap_or(SpacePermissionSet::new()),
+      BuiltinRole::Everyone => space_perms.everyone.unwrap_or(SpacePermissionSet::new()),
+      BuiltinRole::PostOwner => {
+        if let Some(post_owner) = space_perms.post_owner {
+          let mut converted_permissions: SpacePermissionSet = SpacePermissionSet::new();
+
+          for permission in post_owner.iter() {
+            converted_permissions.insert(permission.clone().into());
+          }
+
+          converted_permissions
+        } else {
+          SpacePermissionSet::new()
+        }
+      }
+    }
+  }
+
+  fn get_post_permissions_set_by_role(&self, space_perms: PostPermissions) -> PostPermissionSet {
+    match self {
+      BuiltinRole::None => space_perms.none.unwrap_or(PostPermissionSet::new()),
+      BuiltinRole::SpaceOwner => space_perms.space_owner.unwrap_or(PostPermissionSet::new()),
+      BuiltinRole::Follower => space_perms.follower.unwrap_or(PostPermissionSet::new()),
+      BuiltinRole::Everyone => space_perms.everyone.unwrap_or(PostPermissionSet::new()),
+      BuiltinRole::PostOwner => space_perms.post_owner.unwrap_or(PostPermissionSet::new())
+    }
+  }
+}*/
+
+pub type SpacePermissionSet = BTreeSet<SpacePermission>;
+
+pub type PostPermissionSet = BTreeSet<PostPermission>;
 
 /// The pallet's configuration trait.
 pub trait Trait: system::Trait {
-  type DefaultSpacePermissions: Get<SpacePermissions>;
-  type DefaultPostPermissions: Get<PostPermissions>;
+  type DefaultSpacePermissions: Get<SpacePermissionSet>;
+  type DefaultPostPermissions: Get<PostPermissionSet>;
 }
 
 decl_module! {
   pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-    const DefaultSpacePermissions: SpacePermissions = T::DefaultSpacePermissions::get();
-    const DefaultPostPermissions: PostPermissions = T::DefaultPostPermissions::get();
+    const DefaultSpacePermissions: SpacePermissionSet = T::DefaultSpacePermissions::get();
+    const DefaultPostPermissions: PostPermissionSet = T::DefaultPostPermissions::get();
   }
 }
 
@@ -170,6 +259,12 @@ impl<T: Trait> Module<T> {
 
     // Try to find a permission in space overrides:
     let mut role_opt = space_permissions_context.space_perms.get(&permission);
+    /*let mut role_opt: Option<BuiltinRole> = None;
+    for (builtin_role, space_perms_set) in space_permissions_context.space_perms.into_iter().enumerate() {
+      if let Some(_) = space_perms_set.get(&permission) {
+        role_opt = Option::from(builtin_role.into());
+      }
+    }*/
 
     // Look into default space permissions,
     // if there is no permission override for this space:
@@ -178,8 +273,9 @@ impl<T: Trait> Module<T> {
       role_opt = default_perms.get(&permission);
     }
 
-    Self::is_user_in_role(
+    Self::has_user_a_role_permission(
       &space_permissions_context,
+      None,
       role_opt
     )
   }
@@ -209,25 +305,33 @@ impl<T: Trait> Module<T> {
       role_opt = default_perms.get(&permission);
     }
 
-    Self::is_user_in_role(
+    Self::has_user_a_role_permission(
       &space_permissions_context,
+      Some(&post_permissions_context),
       role_opt
     )
   }
 
-  pub fn is_user_in_role(
+  fn has_user_a_role_permission(
     space_permissions_context: &SpacePermissionsContext,
+    post_permissions_context_opt: Option<&PostPermissionsContext>,
     role_to_check: Option<&BuiltinRole>,
   ) -> Option<BuiltinRole> {
 
-    let is_owner = space_permissions_context.is_space_owner;
+    let is_space_owner = space_permissions_context.is_space_owner;
     let is_follower = space_permissions_context.is_space_follower;
+
+    let mut is_post_owner = false;
+    if let Some(post_permissions_context) = post_permissions_context_opt {
+      is_post_owner = post_permissions_context.is_post_owner;
+    }
 
     if let Some(role) = role_to_check {
       if *role == BuiltinRole::None ||
-        *role == BuiltinRole::Owner && is_owner ||
-        *role == BuiltinRole::Follower && (is_owner || is_follower) ||
-        *role == BuiltinRole::Everyone
+        *role == BuiltinRole::SpaceOwner && is_space_owner ||
+        *role == BuiltinRole::Follower && (is_space_owner || is_follower) ||
+        *role == BuiltinRole::Everyone ||
+        *role == BuiltinRole::PostOwner && is_post_owner
       {
         return Some(role.clone());
       }
