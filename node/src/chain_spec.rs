073524dc1b5178f1e459b68d3bfeacec5971a80d
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-08-08 12:57:10 +0300

    WIP: add approximate staging config
    Warning: keys specified in staging config are only for testing purposes

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,4 +1,4 @@
-use sp_core::{Pair, Public, sr25519};
+use sp_core::{Pair, Public, sr25519, crypto::UncheckedInto};
 use subsocial_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig, UtilsConfig,
 	SudoConfig, SystemConfig, WASM_BINARY, Signature, constants::currency::DOLLARS,
@@ -8,7 +8,7 @@ use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{Verify, IdentifyAccount};
 use sc_service::{ChainType, Properties};
 use sc_telemetry::TelemetryEndpoints;
-// use hex_literal::hex;
+use hex_literal::hex;
 
 // Note this is the URL for the telemetry server
 const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -114,17 +114,58 @@ pub fn local_testnet_config() -> ChainSpec {
 
 pub fn subsocial_staging_config() -> ChainSpec {
 	ChainSpec::from_genesis(
-		"Subsocial Cougar Testnet",
-		"subsocial_testnet",
+		"Subsocial",
+		"subsocial",
 		ChainType::Live,
 		|| testnet_genesis(
 			vec![
-				authority_keys_from_seed("Alice"),
-				authority_keys_from_seed("Bob"),
+				(
+					/* AuraId */
+					hex!["d4f4482d82913b5a4ef195bba7ec5567ee04b6ea784139c04ee5e77530670149"].unchecked_into(),
+					/* GrandpaId */
+					hex!["419c48625508cc74c8c125dd0132158d3583b479975e516c3c2cc1457d11a7c5"].unchecked_into()
+				),
+				(
+					/* AuraId */
+					hex!["640f97526d653b620d1ccbdefa4cb212cc0dfc76f77a0f631f96f009d986464b"].unchecked_into(),
+					/* GrandpaId */
+					hex!["b553650f5032ecadba5df3bd35e8d280bc6f5cdff1beda8c504d6cfecc89c63a"].unchecked_into()
+				),
 			],
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			vec![],
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
+			hex!["a8d5b1558ee63ed2c55c8fb71afd2cbe7a2f61c0fc2dbab741ca652ecf6a3f45"].into(),
+			vec![
+				(
+					/* Account */
+					hex!["a8d5b1558ee63ed2c55c8fb71afd2cbe7a2f61c0fc2dbab741ca652ecf6a3f45"].into(),
+					/* Balance */
+					10_000
+				),
+				(
+					/* Account */
+					hex!["24d6d996a8bb42a63904afc36d610986e8d502f65898da62cb281cfe7f23b02f"].into(),
+					/* Balance */
+					2_497_500
+				),
+				(
+					/* Account */
+					hex!["24d6d8fc5d051fd471e275f14c83e95287d2b863e4cc802de1f78dea06c6ca78"].into(),
+					/* Balance */
+					2_497_500
+				),
+				(
+					/* Account */
+					hex!["24d6d901fb0531124040630e52cfd746ef7d037922c4baf290f513dbc3d47d66"].into(),
+					/* Balance */
+					2_497_500
+				),
+				(
+					/* Account */
+					hex!["24d6d22d63313e82f9461281cb69aacad1828dc74273274751fd24333b182c68"].into(),
+					/* Balance */
+					2_497_500
+				),
+			],
+			hex!["fa8f0122f7f950feb5d5513f4ad6d0446402a5c99572801f5dfde0169be1db5e"].into(),
 			true,
 		),
 		vec![],
