41d3a033ba6ff6513baecc4b6bf097578d8fe6c4
Alex Siman <aleksandr.siman@gmail.com>
2020-08-08 16:34:15 +0300

    Fix tabs in constants.rs

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -12,13 +12,13 @@ pub mod currency {
 }
 
 pub mod time {
-    use node_primitives::{Moment, BlockNumber};
+	use node_primitives::{Moment, BlockNumber};
 
-    pub const MILLISECS_PER_BLOCK: Moment = 6000;
-    pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
+	pub const MILLISECS_PER_BLOCK: Moment = 6000;
+	pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
 
-    // These time units are defined in number of blocks.
-    pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-    pub const HOURS: BlockNumber = MINUTES * 60;
-    pub const DAYS: BlockNumber = HOURS * 24;
+	// These time units are defined in number of blocks.
+	pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
+	pub const HOURS: BlockNumber = MINUTES * 60;
+	pub const DAYS: BlockNumber = HOURS * 24;
 }
