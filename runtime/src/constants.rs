c883fea1c534c38d91fa336816a9a05f7dc304a2
Alex Siman <aleksandr.siman@gmail.com>
2020-08-08 15:14:33 +0300

    Change DOLLARS from SMNS / 100 => SMNS

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -2,12 +2,12 @@ pub mod currency {
 	use node_primitives::Balance;
 
 	pub const SMNS: Balance = 1_000_000_000_000;
-	pub const DOLLARS: Balance = SMNS / 100;       // 10_000_000_000
-	pub const CENTS: Balance = DOLLARS / 100;      // 100_000_000
-	pub const MILLICENTS: Balance = CENTS / 1_000; // 100_000
+	pub const DOLLARS: Balance = SMNS;             // 1_000_000_000_000
+	pub const CENTS: Balance = DOLLARS / 100;      // 10_000_000_000
+	pub const MILLICENTS: Balance = CENTS / 1_000; // 10_000_000
 
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
-		items as Balance * 20 * DOLLARS + (bytes as Balance) * 100 * MILLICENTS
+		items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
 	}
 }
 
