3bf61dd7a78d73f09a4eb6c34f3a335e5bde2df2
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-08-17 18:17:45 +0300

    Add types aggregation JS helper script

---
+++ scripts/aggregate_types.js
@@ -0,0 +1,41 @@
+// Reads in the type definitions from all pallets in the runtime and the runtime's own tpes
+// Naively aggregates types and writes them to disk.
+
+const fs = require('fs');
+
+const pallets = [
+  "donations",
+  "moderation",
+  "permissions",
+  "post-history",
+  "posts",
+  "profile-history",
+  "profiles",
+  "reactions",
+  "roles",
+  "scores",
+  "session-keys",
+  "space-history",
+  "spaces",
+  "subscriptions",
+  "utils",
+]
+
+// Types that are native to the runtime itself (ie come from lib.rs)
+// These specifics are from https://polkadot.js.org/api/start/types.extend.html#impact-on-extrinsics
+const runtimeOwnTypes = {
+  "Address": "AccountId",
+  "LookupSource": "AccountId"
+}
+
+// Loop through all pallets aggregating types
+let finalTypes = runtimeOwnTypes;
+let palletTypes;
+for (let dirname of pallets) {
+  let path = `../pallets/${dirname}/types.json`;
+  palletTypes = JSON.parse(fs.readFileSync(path, 'utf8'));
+  finalTypes = {...finalTypes, ...palletTypes};
+}
+
+// Write output to disk
+fs.writeFileSync("../types.json", JSON.stringify(finalTypes, null, 2), 'utf8');
