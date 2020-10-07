afed8f8423b45477c41b6d043e3be67a0175fdd5
Vadyslav Samchuk <sam4uk.v1ad@gmail.com>
2020-10-07 23:48:50 +0300

    WIP: Test RPC what returns the structure

--- pallets/spaces/runtime-api/src/lib.rs
+++ pallets/spaces/runtime-api/src/lib.rs
@@ -4,7 +4,7 @@
 use sp_std::vec::Vec;
 
 use pallet_utils::SpaceId;
-use pallet_spaces::{Trait as SpacesTrait, Space};
+use pallet_spaces::{Trait as SpacesTrait, Space, WhoAndWhen1};
 
 sp_api::decl_runtime_apis! {
     pub trait SpacesApi<T: SpacesTrait> {
@@ -16,6 +16,8 @@ sp_api::decl_runtime_apis! {
 
         fn find_unlisted_space_ids(offset: u64, limit: u64) -> Vec<SpaceId>;
 
-        fn find_public_spaces(offset: u64, limit: u64) -> Vec<Space<T>>;
+        // fn find_public_spaces(offset: u64, limit: u64) -> Vec<Space<T>>;
+
+        fn find_struct() -> Vec<WhoAndWhen1<T>>;
     }
 }
