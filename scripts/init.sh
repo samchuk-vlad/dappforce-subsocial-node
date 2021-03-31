5e503bed50110d1cfadcade309a114358a715650
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-03-31 17:31:11 +0300

    Fix code after merge

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,6 +9,4 @@ if [ -z $CI_PROJECT_NAME ] ; then
    rustup update stable
 fi
 
-rustup install nightly-2020-07-02
-rustup target add wasm32-unknown-unknown --toolchain nightly-2020-07-02
-rustup default nightly-2020-07-02
+rustup target add wasm32-unknown-unknown --toolchain nightly
